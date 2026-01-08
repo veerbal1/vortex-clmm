1c8f0a746e99e76b2a2c4616ba7422f1b8cf7a74
veerbal1 <veerbalsingh1@gmail.com>
2026-01-08 18:20:03 +0530

    feat: add update methods for WhirlpoolsConfig to modify fee authorities and default protocol fee rate with validation

--- programs/vortex-clmm/src/state/config.rs
+++ programs/vortex-clmm/src/state/config.rs
@@ -31,7 +31,38 @@ impl WhirlpoolsConfig {
         self.collect_protocol_fees_authority = collect_protocol_fees_authority;
         self.reward_emissions_super_authority = reward_emissions_super_authority;
         self.default_protocol_fee_rate = default_protocol_fee_rate;
+        self.feature_flags = 0;
 
+        Ok(())
+    }
+
+    pub fn update_fee_authority(&mut self, fee_authority: Pubkey) -> Result<()> {
+        self.fee_authority = fee_authority;
+        Ok(())
+    }
+
+    pub fn update_collect_protocol_fees_authority(
+        &mut self,
+        collect_protocol_fees_authority: Pubkey,
+    ) -> Result<()> {
+        self.collect_protocol_fees_authority = collect_protocol_fees_authority;
+        Ok(())
+    }
+
+    pub fn update_reward_emissions_super_authority(&mut self, authority: Pubkey) -> Result<()> {
+        self.reward_emissions_super_authority = authority;
+        Ok(())
+    }
+
+    pub fn update_default_protocol_fee_rate(
+        &mut self,
+        default_protocol_fee_rate: u16,
+    ) -> Result<()> {
+        require!(
+            default_protocol_fee_rate <= MAX_PROTOCOL_FEE_RATE,
+            VortexError::InvalidProtocolFeeRate
+        );
+        self.default_protocol_fee_rate = default_protocol_fee_rate;
         Ok(())
     }
 }
