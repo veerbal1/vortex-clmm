d5176ad0fe2b1fe3032b37fd76ce1948169f9a06
veerbal1 <veerbalsingh1@gmail.com>
2026-01-10 16:14:35 +0530

    feat: reorganize module imports in state.rs to streamline access to config, fee_tier, oracle, position, tick, tick_array, and whirlpool, enhancing code clarity and maintainability

--- programs/vortex-clmm/src/state/mod.rs
+++ programs/vortex-clmm/src/state/mod.rs
@@ -1,7 +1,15 @@
 pub mod config;
 pub mod fee_tier;
+pub mod oracle;
+pub mod position;
 pub mod tick;
 pub mod tick_array;
 pub mod whirlpool;
-pub mod position;
-pub mod oracle;
+
+pub use config::*;
+pub use fee_tier::*;
+pub use oracle::*;
+pub use position::*;
+pub use tick::*;
+pub use tick_array::*;
+pub use whirlpool::*;
